98f4371ffd1dad442f9d0eba3dbd45687739eb81
losfair <zhy20000919@hotmail.com>
2018-10-21 11:44:41 +0800

    main: Load config & start periodic query!

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,11 @@ module Main where
 
 import qualified GitDeployer.Config
 import qualified GitDeployer.Local
+import qualified GitDeployer.Query
+import qualified System.Environment as Env
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    (cfgPath:_) <- Env.getArgs
+    cfg <- GitDeployer.Config.loadFromYamlFile cfgPath
+    GitDeployer.Query.periodicallyQueryAll cfg
