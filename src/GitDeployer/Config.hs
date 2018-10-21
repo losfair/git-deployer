2efc0a55b6b7f58fc81a60df3a9638398a0bbdbd
losfair <zhy20000919@hotmail.com>
2018-10-21 11:43:33 +0800

    config: Update fields

--- src/GitDeployer/Config.hs
+++ src/GitDeployer/Config.hs
@@ -6,23 +6,20 @@ import qualified Data.Aeson as Aeson
 import qualified Data.Yaml as Yaml
 import GHC.Generics
 
-data Config = Config { repos :: RepoConfig } deriving (Show, Generic)
+data Config = Config { repos :: [RepoConfig] } deriving (Show, Generic)
 data RepoConfig = RepoConfig {
-    provider :: Provider,
-    remotePath :: String,
+    provider :: String,
+    providerToken :: String,
+    remoteRepo :: String,
     branch :: String,
     localPath :: String
 } deriving (Show, Generic)
-data Provider = GitHub | GitLab deriving (Show, Generic)
 
 instance Aeson.ToJSON Config
 instance Aeson.FromJSON Config
 
 instance Aeson.ToJSON RepoConfig
 instance Aeson.FromJSON RepoConfig
-
-instance Aeson.ToJSON Provider
-instance Aeson.FromJSON Provider
 
 loadFromJsonFile :: String -> IO Config
 loadFromJsonFile path = do
@@ -33,3 +30,7 @@ loadFromJsonFile path = do
 
 loadFromYamlFile :: String -> IO Config
 loadFromYamlFile path = Yaml.decodeFileThrow path
+
+remoteUrl :: RepoConfig -> String
+remoteUrl repo = case provider repo of
+    "GitHub" -> "https://github.com/" ++ remoteRepo repo
